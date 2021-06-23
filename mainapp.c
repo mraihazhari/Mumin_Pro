e1a5bdd56d46a46a1d86bac5e309660b5ec3ecd9
Muhammad Raihan <80392807+mraihazhari@users.noreply.github.com>
2021-06-23 09:27:57 +0000

    Coba test di repl

--- mainapp.c
+++ mainapp.c
@@ -90,7 +90,7 @@ int main(){
 	menu = 0;
 	mutabaah = 0;
 	id = 0;
-
+  printf("GitHub test");
 	//struct user dalam fungsi ini dapat dipanggil melalui pointer *userptr
 	User *userptr;
 	userptr = (User*) calloc(1, 2 * sizeof(User));
